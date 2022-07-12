93592bf03629a5132535730ca418c9c392aa994f
root <root@ip-172-31-84-31.ec2.internal>
2022-07-12 10:44:09 +0000

    adding

--- system_update/post_update_check.sh
+++ system_update/post_update_check.sh
@@ -1,18 +1,28 @@
 work_dir="/tmp/post"
 
+echo "------------------------------"
+echo "Host details.."
+echo "----"
+hostname;  ec2metadata --instance-id
+echo "------------------------------"
+
 echo "System Uptime"
+echo "----"
 uptime
-echo "--------------------------"
+echo "------------------------------"
 
 echo "Check if there is any package upgradable...."
+echo "----"
 sudo apt-get update
-echo "-------------------------"
+echo "------------------------------"
 
 echo "Checking the service Status..............."
+echo "----"
 for i in `cat $work_dir/services_lt |awk '{print $7}'  |cut -d "/" -f2 |cut -d: -f1`; do echo "Service $i `systemctl is-active $i`"; done
-echo "--------------------------------"
+echo "------------------------------"
 
 echo "Verifying the mount points..................."
+echo "----"
 df -h |awk '{print $6}' > $work_dir/post_mnt
 cmp --silent  $work_dir/pre_mnt $work_dir/post_mnt  && echo "Successfully mounted \n  Current mount details: `cat $work_dir/post_mnt`"  || echo "ERROR: There is a CONFLICT in mount points!!!!\n ----- \n Current_Details  -      Required_Details \n `paste  $work_dir/post_mnt  $work_dir/pre_mnt`"
-
+echo "------------------------------"
