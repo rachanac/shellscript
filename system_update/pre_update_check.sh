2f1208d563280ff1be118c42cb5edd57f1d617f5
gowriand <95476026+gowriand@users.noreply.github.com>
2022-07-12 16:07:02 +0530

    Create pre_update_check.sh

---
+++ system_update/pre_update_check.sh
@@ -0,0 +1,17 @@
+work_dir="/tmp/post"
+
+if [ ! -d $work_dir ]
+then
+  mkdir $work_dir
+fi
+
+
+echo "Following are the services listening..."
+netstat -plnt | grep -v tcp6 |grep tcp  > $work_dir/services_lt
+cat $work_dir/services_lt |awk '{print $7}'  |cut -d "/" -f2 |cut -d: -f1
+echo "-----------------------"
+
+echo "Current Mount point details...."
+df -h |awk '{print $6}' > $work_dir/pre_mnt 
+cat $work_dir/pre_mnt
+echo "------------------"
